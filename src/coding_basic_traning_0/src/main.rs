f415d29b05e7a304745b47bb9848abe9a726f52d
gong-yeongbin <youngbin816@gmail.com>
2025-04-25 14:56:49 +0900

    문자열 돌리기

--- src/coding_basic_traning_0/src/main.rs
+++ src/coding_basic_traning_0/src/main.rs
@@ -1,5 +1,4 @@
 use std::io::{self, BufRead};
-use std::process::exit;
 
 fn is_alphabetic(str: &str) -> bool {
     let text = str.clone().trim().replace(" ", "");
@@ -219,48 +218,54 @@ fn main() {
     출력 #2
         HelloWorld!
     */
-    println!("##### 문자열 붙여서 출력하기 #####");
+    // println!("##### 문자열 붙여서 출력하기 #####");
+    // let mut args = String::new();
+    //
+    // println!("입력 #");
+    // io::stdin().read_line(&mut args).unwrap();
+    //
+    // let input: Vec<&str> = args.trim().split_whitespace().collect();
+    // let str1 = input[0];
+    // let str2 = input[1];
+    //
+    // if str1.len() < 1 || str2.len() > 10 {
+    //     println!("- 1 ≤ str1, str2의 길이 ≤ 10 error!");
+    //     exit(0);
+    // }
+    //
+    // let mut result = str1.clone().to_string();
+    // result.push_str(str2);
+    //
+    // println!("출력 #");
+    // println!("{}", result);
+
+    /*
+    문자열 돌리기
+    문제 설명
+        문자열 str이 주어집니다.
+        문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+    제한사항
+        - 1 ≤ str의 길이 ≤ 10
+    입출력 예
+    입력 #1
+        abcde
+    출력 #1
+        a
+        b
+        c
+        d
+        e
+    */
+    println!("##### 문자열 돌리기 #####");
     let mut args = String::new();
 
     println!("입력 #");
     io::stdin().read_line(&mut args).unwrap();
 
-    let input: Vec<&str> = args.trim().split_whitespace().collect();
-    let str1 = input[0];
-    let str2 = input[1];
-
-    if str1.len() < 1 || str2.len() > 10 {
-        println!("- 1 ≤ str1, str2의 길이 ≤ 10 error!");
-        exit(0);
-    }
-
-    let mut result = str1.clone().to_string();
-    result.push_str(str2);
-    
     println!("출력 #");
-    println!("{}", result);
-
-    /*
-    문자열 돌리기
-    문제 설명
-문자열 str이 주어집니다.
-문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
-
-제한사항
-1 ≤ str의 길이 ≤ 10
-
-입출력 예
-입력 #1
-
-abcde
-출력 #1
-
-a
-b
-c
-d
-e
-*/
+    for c in args.trim().chars() {
+        println!("{c}");
+    }
 }
 
 
