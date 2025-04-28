e3b0ad38972c88eb488268681de4ac97c873e994
gong-yeongbin <youngbin816@gmail.com>
2025-04-29 08:21:46 +0900

    두 수의 연산값 비교하기

--- src/coding_basic_traning_0/src/main.rs
+++ src/coding_basic_traning_0/src/main.rs
@@ -95,6 +95,18 @@ fn is_mix_str_verification(str: &str) -> Result<(), String> {
 //     }
 //     result
 // }
+// fn solution(a: &str, b: &str) -> String {
+//     let ab_string = format!("{}{}", a, b);
+//     let ba_string = format!("{}{}", b, a);
+//     let ab_i32 = ab_string.parse::<i32>().unwrap();
+//     let ba_i32 = ba_string.parse::<i32>().unwrap();
+//
+//     if ab_i32 > ba_i32 {
+//         ab_string
+//     } else {
+//         ba_string
+//     }
+// }
 
 fn main() {
     /*
@@ -537,33 +549,65 @@ fn main() {
     입출력 예 #2
         a ⊕ b = 898 이고, b ⊕ a = 889 입니다. 둘 중 더 큰 값은 898 이므로 898을 return 합니다.
     */
-    println!("##### 더 크게 합치기 #####");
+    // println!("##### 더 크게 합치기 #####");
+    // let mut buffer = String::new();
+    //
+    // println!("입력 #");
+    // io::stdin().read_line(&mut buffer).unwrap();
+    //
+    // let input: Vec<&str> = buffer.split_whitespace().collect();
+    // let a = input[0];
+    // let b = input[1];
+    //
+    // let result = solution(&a, &b);
+    // println!("출력 #");
+    // println!("{result}");
+
+    /*
+    두 수의 연산값 비교하기
+    문제 설명
+    연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+        12 ⊕ 3 = 123
+        3 ⊕ 12 = 312
+    양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
+    단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
+    제한사항
+        1 ≤ a, b < 10,000
+    입출력 예
+        a	b	result
+        2	91	364
+        91	2	912
+    입출력 예 설명
+    입출력 예 #1
+        a ⊕ b = 291 이고, 2 * a * b = 364 입니다. 둘 중 더 큰 값은 364 이므로 364를 return 합니다.
+    입출력 예 #2
+        a ⊕ b = 912 이고, 2 * a * b = 364 입니다. 둘 중 더 큰 값은 912 이므로 912를 return 합니다.
+    */
+    println!("##### 두 수의 연산값 비교하기 #####");
     let mut buffer = String::new();
 
     println!("입력 #");
     io::stdin().read_line(&mut buffer).unwrap();
-
     let input: Vec<&str> = buffer.split_whitespace().collect();
-    let a = input[0];
-    let b = input[1];
+    let a = input[0].parse::<i32>().unwrap();
+    let b = input[1].parse::<i32>().unwrap();
 
     let result = solution(&a, &b);
-    println!("출력 #");
+    println!("결과 #");
     println!("{result}");
 }
 
-fn solution(a: &str, b: &str) -> String {
-    let ab_string = format!("{}{}", a, b);
-    let ba_string = format!("{}{}", b, a);
-    let ab_i32 = ab_string.parse::<i32>().unwrap();
-    let ba_i32 = ba_string.parse::<i32>().unwrap();
+fn solution(a: &i32, b: &i32) -> i32 {
+    let concat_ab = format!("{a}{b}").parse::<i32>().unwrap();
+    let calculate_ab = 2 * a * b;
 
-    if ab_i32 > ba_i32 {
-        ab_string
+    if concat_ab > calculate_ab {
+        concat_ab
     } else {
-        ba_string
+        calculate_ab
     }
 }
+
 
 
 
