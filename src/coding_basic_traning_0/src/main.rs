b2e765727720421aae45c9b33abab9336c2853bf
gong-yeongbin <youngbin816@gmail.com>
2025-04-28 16:13:21 +0900

    문자열 곱하기

--- src/coding_basic_traning_0/src/main.rs
+++ src/coding_basic_traning_0/src/main.rs
@@ -75,6 +75,16 @@ fn is_mix_str_verification(str: &str) -> Result<(), String> {
     Ok(())
 }
 
+// fn solution(str: Vec<&str>) -> String {
+//     let mut result = String::new();
+//
+//     for s in str {
+//         result.push_str(s)
+//     }
+//
+//     result
+// }
+
 fn main() {
     /*
     문자열 출력하기
@@ -457,27 +467,57 @@ fn main() {
         arr             result
         ["a","b","c"]	"abc"
     */
-    println!("##### 문자 리스트를 문자열로 변환하기 #####");
+    // println!("##### 문자 리스트를 문자열로 변환하기 #####");
+    //
+    // println!("입력 #");
+    // let mut buffer = String::new();
+    // io::stdin().read_line(&mut buffer).unwrap();
+    //
+    // let input: Vec<&str> = buffer.split_whitespace().collect();
+    // let result = solution(input);
+    // println!("출력 #");
+    // println!("{}", result);
 
-    println!("입력 #");
+    /*
+    문자열 곱하기
+    문제 설명
+        문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+    제한사항
+        - 1 ≤ my_string의 길이 ≤ 100
+        - my_string은 영소문자로만 이루어져 있습니다.
+        - 1 ≤ k ≤ 100
+    입출력 예
+        my_string	k	result
+        "string"	3	"stringstringstring"
+        "love"	    10	"lovelovelovelovelovelovelovelovelovelove"
+    입출력 예 설명
+    입출력 예 #1
+        예제 1번의 my_string은 "string"이고 이를 3번 반복한 문자열은 "stringstringstring"이므로 이를 return 합니다.
+    입출력 예 #2
+        예제 2번의 my_string은 "love"이고 이를 10번 반복한 문자열은 "lovelovelovelovelovelovelovelovelovelove"이므로 이를 return 합니다.
+    */
+    println!("##### 문자열 곱하기 #####");
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
 
     let input: Vec<&str> = buffer.split_whitespace().collect();
     let result = solution(input);
     println!("출력 #");
-    println!("{}", result);
+    println!("{result}");
 }
 
-fn solution(str: Vec<&str>) -> String {
+fn solution(input: Vec<&str>) -> String {
     let mut result = String::new();
+    let my_string = input[0];
+    let k = input[1].parse::<usize>().unwrap();
 
-    for s in str {
-        result.push_str(s)
+    for i in 0..k {
+        result.push_str(my_string);
     }
-
     result
 }
+
+
 
 
 
