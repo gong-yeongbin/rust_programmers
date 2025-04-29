82f2a09f5b82183bf32c64d48652bdba36eab4fc
gong-yeongbin <youngbin816@gmail.com>
2025-04-29 13:00:49 +0900

    홀짝에 따라 다른 값 반환하기

--- src/coding_basic_traning_0/src/main.rs
+++ src/coding_basic_traning_0/src/main.rs
@@ -126,6 +126,13 @@ fn is_mix_str_verification(str: &str) -> Result<(), String> {
 //         0
 //     }
 // }
+// fn solution(number: &i32, n: &i32, m: &i32) -> i32 {
+//     if number % n == 0 && number % m == 0 {
+//         1
+//     } else {
+//         0
+//     }
+// }
 fn main() {
     /*
     문자열 출력하기
@@ -661,30 +668,70 @@ fn main() {
     입출력 예 #2
         55는 5의 배수이지만 10의 배수가 아니기 때문에 0을 return합니다.
     */
-    println!("##### 공배수 #####");
+    // println!("##### 공배수 #####");
+    // let mut buffer = String::new();
+    //
+    // println!("입력 #");
+    // io::stdin().read_line(&mut buffer).unwrap();
+    // let input: Vec<&str> = buffer.split_whitespace().collect();
+    //
+    // let number: i32 = input[0].parse::<i32>().unwrap();
+    // let n: i32 = input[0].parse::<i32>().unwrap();
+    // let m: i32 = input[0].parse::<i32>().unwrap();
+    //
+    // let result = solution(&number, &n, &m);
+    // println!("출력 #");
+    // println!("{result}");
+
+    /*
+    홀짝에 따라 다른 값 반환하기
+    문제 설명
+    양의 정수 n이 매개변수로 주어질 때, n이 홀수라면 n 이하의 홀수인 모든 양의 정수의 합을 return 하고 n이 짝수라면 n 이하의 짝수인 모든 양의 정수의 제곱의 합을 return 하는 solution 함수를 작성해 주세요.
+    제한사항
+        1 ≤ n ≤ 100
+    입출력 예
+        n	result
+        7	16
+        10	220
+    입출력 예 설명
+    입출력 예 #1
+        예제 1번의 n은 7로 홀수입니다. 7 이하의 모든 양의 홀수는 1, 3, 5, 7이고 이들의 합인 1 + 3 + 5 + 7 = 16을 return 합니다.
+    입출력 예 #2
+        예제 2번의 n은 10으로 짝수입니다. 10 이하의 모든 양의 짝수는 2, 4, 6, 8, 10이고 이들의 제곱의 합인 22 + 42 + 62 + 82 + 102 = 4 + 16 + 36 + 64 + 100 = 220을 return 합니다.
+    */
+    println!("##### 홀짝에 따라 다른 값 반환하기 #####");
     let mut buffer = String::new();
 
     println!("입력 #");
     io::stdin().read_line(&mut buffer).unwrap();
+
     let input: Vec<&str> = buffer.split_whitespace().collect();
+    let n = input[0].parse::<i32>().unwrap();
 
-    let number: i32 = input[0].parse::<i32>().unwrap();
-    let n: i32 = input[0].parse::<i32>().unwrap();
-    let m: i32 = input[0].parse::<i32>().unwrap();
-
-    let result = solution(&number, &n, &m);
+    let result = solution(&n);
     println!("출력 #");
     println!("{result}");
 }
 
-fn solution(number: &i32, n: &i32, m: &i32) -> i32 {
-    if number % n == 0 && number % m == 0 {
-        1
-    } else {
-        0
-    }
-}
+fn solution(n: &i32) -> i32 {
+    let mut num = n.clone();
+    let mut result: Vec<i32> = Vec::new();
+    let last_num = if num % 2 > 0 { 1 } else { 2 };
 
+    loop {
+        if last_num == 1 {
+            result.push(num);
+        } else {
+            result.push(num.pow(2));
+        }
+
+        num -= 2;
+
+        if num < last_num { break; }
+    }
+
+    result.iter().sum()
+}
 
 
 
